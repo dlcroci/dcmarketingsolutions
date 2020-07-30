05ed678ec7eaa368ac5b35d6481aab72b9f935f8
Daniel Croci <dlcroci@hotmail.com>
2020-07-30 20:25:44 -0300

    Version 1.6

--- wp-content/themes/dcmarketingsolutions/archive-contacto.php
+++ wp-content/themes/dcmarketingsolutions/archive-contacto.php
@@ -20,7 +20,7 @@ get_header();
                             <!-- <br> -->
                             <!-- formulario de contacto -->
 
-                                <form action="get_theme_file_uri('enviar.php')" method="post" class="form-consulta"> 
+                                <form action="<?php enviar(); ?>" method="post" class="form-consulta"> 
                                     <label>Nombre y apellido: <span>*</span>
                                         <input type="text" name="nombre" placeholder="Nombre y apellido" class="campo-form" required>
                                     </label>
@@ -44,7 +44,34 @@ get_header();
             </div>
         </section>
     </main>
+<?php
+    function enviar() {
+    $remitente = $_POST['email'];
+    $destinatario = 'dlcroci@hotmail.com'; // en esta línea va el mail del destinatario.
+    $asunto = 'Consulta para DC Marketing Solutions'; // acá se puede modificar el asunto del mail
+    if (!$_POST){
     
+    }else{
+        
+        $cuerpo = "Nombre y apellido: " . $_POST["nombre"] . "\r\n"; 
+        $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
+        $cuerpo .= "Consulta: " . $_POST["consulta"] . "\r\n";
+        //las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo. 
+        // Si se agrega un campo al formulario, hay que agregarlo acá.
+
+        $headers  = "MIME-Version: 1.0\n";
+        $headers .= "Content-type: text/plain; charset=utf-8\n";
+        $headers .= "X-Priority: 3\n";
+        $headers .= "X-MSMail-Priority: Normal\n";
+        $headers .= "X-Mailer: php\n";
+        $headers .= "From: \"".$_POST['nombre']." ".$_POST['apellido']."\" <".$remitente.">\n";
+
+        mail($destinatario, $asunto, $cuerpo, $headers);
+        
+        // include 'confirmar.html; //se debe crear un html que confirma el envío
+    }
+    };
+?>
 <?php get_footer();
 
 ?>
