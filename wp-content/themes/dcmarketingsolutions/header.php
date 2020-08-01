be3fbe8d443f67e508dd7ab8c25dbd6e3058f436
Daniel Croci <dlcroci@hotmail.com>
2020-07-31 23:51:28 -0300

    Borrado de carpeta favicon

--- wp-content/themes/dcmarketingsolutions/header.php
+++ wp-content/themes/dcmarketingsolutions/header.php
@@ -13,14 +13,6 @@
     gtag('config', 'UA-154738663-2');
     </script>
 
-    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-    <link rel="manifest" href="favicon/site.webmanifest">
-    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#296c7f">
-    <meta name="msapplication-TileColor" content="#00aba9">
-    <meta name="theme-color" content="#ffffff">
-
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
