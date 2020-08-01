11d2dff824975fd50174c5a1c2ff0b859dce0e84
Daniel Croci <dlcroci@hotmail.com>
2020-07-31 23:30:52 -0300

    Favicon

--- wp-content/themes/dcmarketingsolutions/header.php
+++ wp-content/themes/dcmarketingsolutions/header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="keywords" content="marketing, digital, social, community manager, ecommerce, diseño de páginas web">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-154738663-2"></script>
@@ -11,6 +12,14 @@
     gtag('js', new Date());
     gtag('config', 'UA-154738663-2');
     </script>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#296c7f">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="theme-color" content="#ffffff">
 
     <?php wp_head(); ?>
   </head>
