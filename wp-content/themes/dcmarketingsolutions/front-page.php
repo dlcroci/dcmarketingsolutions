06bd4a59894ac5627326c4a05cafe20b0422aeda
Daniel Croci <dlcroci@hotmail.com>
2020-09-08 11:56:16 -0300

    ajustes

--- wp-content/themes/dcmarketingsolutions/front-page.php
+++ wp-content/themes/dcmarketingsolutions/front-page.php
@@ -37,7 +37,7 @@
                                     <a style="text-decoration: none" href=" ">
                                         <h3 class="h2h3 t2">Social Media</h3>
                                     </a>
-                                    <p>Community management. Creación y promoción de contenidos digitales.</p>
+                                    <p style="margin: 0; line-height: 1.2;">Community management. Creación y promoción de contenidos digitales.</p>
                                 </div>
                                 <div class="item_8 ">
                                     <div class="publicidad-online-logo"></div>
@@ -46,7 +46,7 @@
                                     <a style="text-decoration: none" href=" ">
                                         <h3 class="h2h3 t2">Inbound Marketing</h3>
                                     </a>
-                                    <p>Atrae y convence con contenido de valor a tus clientes ideales. Especialistas en SEO y SEM.</p>
+                                    <p style="margin: 0; line-height: 1.2;">Atrae y convence con contenido de valor a tus clientes ideales. Especialistas en SEO y SEM.</p>
                                 </div>
                                 <div class="item_10">
                                     <div class="diseño-web-logo"></div>
@@ -55,7 +55,7 @@
                                     <a style="text-decoration: none" href=" ">
                                         <h3 class="h2h3">Diseño Web</h3>
                                     </a>
-                                    <p>Diseño y desarrollo de sitios web institucionales y comerciales.</p>
+                                    <p style="margin: 0; line-height: 1.2;">Diseño y desarrollo de sitios web institucionales y comerciales.</p>
                                 </div>
                                 <div class="item_12 ">
                                     <div class="ecommerce-logo"></div>
@@ -64,7 +64,7 @@
                                     <a style="text-decoration: none" href=" ">
                                         <h3 class="h2h3 t2">Ecommerce</h3>
                                     </a>
-                                    <p>La experiencia para implementar soluciones de Ecommerce escalables y rentables que cumplan con sus objetivos.</p>
+                                    <p style="margin: 0; line-height: 1.2;">La experiencia para implementar soluciones de Ecommerce escalables y rentables que cumplan con sus objetivos.</p>
                                 </div>
                             </div>
                             <div class="grid-container4 <!--container-quienes-somos-->" id="id_queremos_ayudarte">
