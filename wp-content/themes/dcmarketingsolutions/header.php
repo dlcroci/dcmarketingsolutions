05564b0ab5d83413c3272c58cf54ef38711ed360
Daniel Croci <dlcroci@hotmail.com>
2020-09-10 17:39:54 -0300

    Ajustes

--- wp-content/themes/dcmarketingsolutions/header.php
+++ wp-content/themes/dcmarketingsolutions/header.php
@@ -14,7 +14,7 @@
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
-        <header class="site-header">       
+        <header class="site-header" style="max-width:1366px !important">       
             <div class="container">
                 <div class="grid-container1">
                     <div class="item_1">
